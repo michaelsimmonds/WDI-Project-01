fa28a163e7b3bb160c0686e2d5bfa627719ad291
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-03 19:47:54 +0000

    Added code to ensure that the player cannot leave the boundaries of the page

--- js/app.js
+++ js/app.js
@@ -5,23 +5,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let player = {
     top: 600,
-    left: 550
+    left: 525
   }
+
+  let lasers = []
 
   document.onkeydown = function(e) {
     if (e.keyCode === 37) {
       console.log('LEFT')
-      player.left -= 10
+      player.left -= 20
+      movePlayer()
     } else if (e.keyCode === 39) {
       console.log('RIGHT')
-      player.left += 10
+      player.left += 20
+      movePlayer()
+    } else if (e.keyCode === 32) {
+      // console.log('shoot')
+      // lasers.push({
+      //   top: player.top,
+      //   left: player.left
+      // })
+      // console.log(lasers)
+      // shoot()
     }
-    movePlayer()
   }
 
   function movePlayer() {
     document.querySelector('.player').style.left = player.left + 'px'
+    boundary()
   }
 
+// Boundary function. Ensures the player does not leave the edges of the page
+  function boundary() {
+    if (player.left < 30) {
+      player.left = 30
+    } else if (player.left > 1020) {
+      player.left = 1020
+    }
+
+  }
+
+  // function shoot() {
+  //   let laser = document.createElement('div')
+  //   laser.className = 'laser'
+  //   for (let i = 0; i < lasers.length; i ++) {
+  //     // laser.innerHtml = `<div class='laser' style:'left:${lasers[i].left}px'></div>`
+  //     laser.top = 600
+  //     laser.right = 550
+  //     console.log('worked')
+  //   }
+  // }
 
 })
