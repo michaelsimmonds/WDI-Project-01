0e5bc0623e15ba19e3363bc2f7fa01049d870de4
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-03 16:28:05 +0000

    Have a player ship that can move right and left

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const h1 = document.querySelector('h1')
-  h1.textContent = 'Hello Word!'
+  // const h1 = document.querySelector('h1')
+  // // h1.textContent = 'Hello Word!'
+  // const player = document.querySelector('.player')
+
+  let player = {
+    top: 600,
+    left: 550
+  }
+
+  document.onkeydown = function(e) {
+    if (e.keyCode === 37) {
+      console.log('LEFT')
+      player.left -= 10
+    } else if (e.keyCode === 39) {
+      console.log('RIGHT')
+      player.left += 10
+    }
+    movePlayer()
+  }
+
+  function movePlayer() {
+    document.querySelector('.player').style.left = player.left + 'px'
+  }
+
+
 })
