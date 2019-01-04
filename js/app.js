02360fab57ccf2ab3a06800c229781db88cdb66a
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-04 14:25:57 +0000

    Replaced earlier code so can now generate laser on click of spacebar. Enables more than one laser at any one time. Have removed laser div from HTML

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  let player = document.querySelector('.player')
+  const player = document.querySelector('.player')
+
   player.left = 550
   player.top = 600
 
-  let laser = document.querySelector('.laser')
-  let lasers = []
+  const lasers = []
+
+  function createLaser() {
+    const laser = document.createElement('div')
+    const gamespace = document.querySelector('.gamespace')
+    gamespace.appendChild(laser)
+    laser.className = 'laser'
+    laser.style.left = player.left + 20 + 'px'
+    laser.style.top = player.top - 30 + 'px'
+    lasers.push(laser)
+    console.log(lasers)
+  }
 
   document.onkeydown = function(e) {
     if (e.keyCode === 37) {
@@ -18,23 +29,25 @@ document.addEventListener('DOMContentLoaded', () => {
       movePlayer()
     } else if (e.keyCode === 32) {
       console.log('fire')
-      shotAppear()
+      createLaser()
     }
   }
 
+  // Move the player left and right //
   function movePlayer() {
     document.querySelector('.player').style.left = player.left + 'px'
     boundary()
   }
 
-  function shotAppear() {
-    laser.style.left = player.left + 20 + 'px'
-    laser.style.top = player.top - 30 + 'px'
-    lasers.push(laser)
-  }
+
+  //  This does not work yet
+  // function moveLaser() {
+  //   console.log(laser)
+  //   laser.style.top += player.top + 20 + 'px'
+  // }
 
 
-// Boundary function. Ensures the player does not leave the edges of the page
+  // Boundary function. Ensures the player does not leave the edges of the page
   function boundary() {
     if (player.left < 30) {
       player.left = 30
@@ -44,12 +57,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
 
-  // function shoot() {
-  //   document.querySelector('.laser').innerHtml = ''
-  //   for (let laser = 0; laser < lasers.length; laser ++) {
-  //     document.querySelector('.laser').innerHtml = `<div class='laser' style:'left:${lasers[laser].left}px; top:${lasers[laser].top}px;'></div>`
-  //     console.log('worked')
-  //   }
+  // function gameLoop() {
+  //   setTimeout(gameLoop, 1000)
+  //   moveLaser()
+  //   laserAppear()
+  // }
+  // gameLoop()
+
+
+
+
+
+  // CODE NOW RENDUNDANT --- MADE A DIFFERENT BIT TO GENERATE THE LASERS ON CLICK
+  // function laserAppear() {
+  //   laser.style.left = player.left + 20 + 'px'
+  //   laser.style.top = player.top - 30 + 'px'
+  //   lasers.push(laser)
+  //   console.log(lasers)
   // }
 
 })
