b6656533f04a22e5202dfe88240bd4764c009d8d
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-04 17:58:44 +0000

    Now have a laser that moves

--- js/app.js
+++ js/app.js
@@ -1,22 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const player = document.querySelector('.player')
-
   player.left = 550
   player.top = 600
 
   const lasers = []
-
-  function createLaser() {
-    const laser = document.createElement('div')
-    const gamespace = document.querySelector('.gamespace')
-    gamespace.appendChild(laser)
-    laser.className = 'laser'
-    laser.style.left = player.left + 20 + 'px'
-    laser.style.top = player.top - 30 + 'px'
-    lasers.push(laser)
-    console.log(lasers)
-  }
+  const aliens = []
+  console.log(lasers)
 
   document.onkeydown = function(e) {
     if (e.keyCode === 37) {
@@ -39,14 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
     boundary()
   }
 
-
-  //  This does not work yet
-  // function moveLaser() {
-  //   console.log(laser)
-  //   laser.style.top += player.top + 20 + 'px'
-  // }
-
-
   // Boundary function. Ensures the player does not leave the edges of the page
   function boundary() {
     if (player.left < 30) {
@@ -54,26 +36,49 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (player.left > 1020) {
       player.left = 1020
     }
-
   }
 
+  function createLaser() {
+    const laser = document.createElement('div')
+    const gamespace = document.querySelector('.gamespace')
+    gamespace.appendChild(laser)
+    laser.className = 'laser'
+    laser.style.left = player.left + 20 + 'px'
+    laser.style.top = player.top - 30 + 'px'
+    lasers.push(laser)
+    console.log(laser)
+    setInterval(function() {
+      const s = 10
+      const movement = laser.offsetTop
+      laser.style.top = (movement - s) + 'px'
+    }, 10)
+  }
+
+  // function moveLaser() {
+  //   for (let i = 0; i <= lasers.length; i++) {
+  //     lasers[i].top += 5
+  //   }
+  // }
+  // moveLaser()
+  //
   // function gameLoop() {
   //   setTimeout(gameLoop, 1000)
   //   moveLaser()
-  //   laserAppear()
+  //   // createLaser()
+  //   console.log('works')
   // }
   // gameLoop()
 
 
-
-
-
-  // CODE NOW RENDUNDANT --- MADE A DIFFERENT BIT TO GENERATE THE LASERS ON CLICK
-  // function laserAppear() {
-  //   laser.style.left = player.left + 20 + 'px'
-  //   laser.style.top = player.top - 30 + 'px'
-  //   lasers.push(laser)
-  //   console.log(lasers)
+  // TRIED TO CREATE ALIEN USING JAVASCRIPT BUT COULDN'T WORK OUT HOW TO DO MORE THAN ONE
+  // function createAlien() {
+  //   const div = document.createElement('div')
+  //   const gamespace = document.querySelector('.gamespace')
+  //   const alien = gamespace.appendChild(div)
+  //   alien.className = 'alien'
+  //   aliens.push(alien)
+  //   alien.style.left += 60 + 'px'
+  //   alien.style.top += 60 + 'px'
+  //   console.log(aliens)
   // }
-
 })
