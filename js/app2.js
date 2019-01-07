fe3b34805935373c46f9def6cb9512f8e67865c8
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-07 01:46:15 +0000

    Have got the aliens moving in formation. No boundry limits yet though

--- js/app2.js
+++ js/app2.js
@@ -8,14 +8,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const alienArray =  []
   const aliensInRow = 6
   let score = 0
+  let direction = 'right'
+  let changeDirection = false
 
-  // CREATES A DIV. CALLED IN INIT FUNCTION
+  // Creates divs that make up the grid. Called in init function.
   function addDivs() {
     const newDiv = document.createElement('div')
     grid.appendChild(newDiv)
   }
 
-  // HANDLES KEYS
+  // Handles the keys
   document.onkeydown = function(e) {
     prevIndex = playerIndex
     if (e.keyCode === 37 && playerIndex > div.length - width*2) {
@@ -45,12 +47,12 @@ document.addEventListener('DOMContentLoaded', () => {
         div[playerLaser].classList.remove('laser')          //this works but I dont know why- error showing
       } else {
         playerLaserArray.push(playerLaser)
-        console.log(playerLaserArray)
+        // console.log(playerLaserArray)
         div[playerLaser].classList.add('laser')
         checkCollision()
         div[playerLaser+width].classList.remove('laser')
-      }           // GLITCHES IF YOU SHOOT INTO TOP RIGHT CORNER FOR A SECOND
-    }, 500)
+      }           // GLITCHES IF YOU SHOOT INTO TOP RIGHT CORNER
+    }, 100)
   }
 
   //Creates alien rows-refactor them at some point
@@ -87,13 +89,61 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // starts the aliens moving and should be refered back to to change direction
+  function gameLoop() {
+    setInterval(function() {
+      if(changeDirection){               //starts as false so these if options are skipped
+        moveAlien('down')
+        if(direction ==='left'){
+          direction ='right'
+        }else{
+          direction ='left'
+        }
+        changeDirection = false
+      }else{
+        moveAlien(direction)        //this starts the directions. it is set to 'right' at the top intiially
+        // checkAlien()                //this check alien boundaries and see whether to change direction
+      }
+    }, 1000)
+  }
+
+  function moveAlien(direction) {
+    for (let i=0; i < alienArray.length; i++) {
+      if (direction === 'left') {
+        alienArray[i] -= 1
+      } else if (direction === 'right') {
+        alienArray[i] += 1
+      } else {                        //don't put set direction to down here
+        alienArray[i] += width
+      }
+    }
+    showAlienMove()
+  }
+
+  function showAlienMove() {
+    for (let i=0; i < div.length; i++) {
+      if(div[i].className === 'alien') {
+        console.log('works')
+        div[i].classList.remove('alien')
+      }
+    }
+    for (let i=0; i < alienArray.length; i++) {
+      div[alienArray[i]].classList.add('alien')
+    }
+  }
+
   //collision. THIS WORKS BUT THINK IT COUNTS THE HITS THREE TIMES TOO MANY TIMES
   function checkCollision() {
     for (let i = 0; i < alienArray.length; i++) {
       for (let j = 0; j < playerLaserArray.length; j++) {
         if (alienArray[i] === playerLaserArray[j]) {
           score += 10
-          console.log(score)
+          alienArray[i].classList.remove('alien')
+          playerLaserArray[j].classList.remove('laser')
+          // playerLaserArray[j] = undefined
+          // alienArray.splice(i, 1)
+          // playerLaserArray.splice(i, 1)
+          // console.log(alienArray)
         }
       }
     }
@@ -107,11 +157,11 @@ document.addEventListener('DOMContentLoaded', () => {
     div = document.querySelectorAll('div')
     playerIndex = (div.length-1) - (width*1.5)
     div[playerIndex].classList.add('player')
-    // createAliens()
     createRowOne()
     createRowTwo()
     createRowThree()
     createRowFour()
+    gameLoop()
   }
 
   init()
